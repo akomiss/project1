298737c616b2f63ee24a2e26e67f82c72df371b6
Andreas <akomissopoulos@gmail.com>
2018-09-20 22:39:10 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,25 @@
+﻿<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php
+ function myFunction() {
+ 	return $_REQUEST["name"];
+ }
+
+ if($_SERVER['REQUEST_METHOD'] == 'POST') {
+ 	echo myFunction();
+ }
+
+ echo $_SERVER['SERVER_SIGNATURE']
+ ?>
+ </body>
+ <body>
+ 	<?php $initSVal = 'abc'; ?>
+ <form method='post' action = '<?php echo $_SERVER["PHP_SELF"]?>'>
+ 	Name: <input type='text' name='name'>
+ 	<input type='submit'>
+ </form>
+ </body>
+</html>
